380cafd027a44e4f5ccfd0a728f93689e5d96f4e
AndreyLuck11 <kkoott2002@gmail.com>
2020-03-07 15:58:47 +0300

    Create Zadanie13.cpp

---
+++ Zadanie13.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+float cooki;
+float candy;
+float bay1;
+float bay2;
+
+
+int main() {
+
+	cout << "Enter the cost of 1 KG of sweets" << endl;
+	cin >> candy;
+	cout << "Enter the cost of 1 KG of cookies" << endl;
+	cin >> cooki;
+	bay1 = candy * 0.3 + cooki * 0.4;
+	bay2 = 3 * (cooki * 2 + candy * 1.8);
+
+	cout << "Purchase value 1 =" << bay1 << endl;
+	cout << "Purchase value 2 =" << bay2 << endl;
+
+	getchar();  // ожидание ввода
+	getchar();
+	return 0;
+
+}
