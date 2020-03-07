605a44c05229045898338f8342c1104f50ff1978
AndreyLuck11 <kkoott2002@gmail.com>
2020-03-07 15:58:51 +0300

    Create Zadanie8.cpp

---
+++ Zadanie8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+float x = -2.34;
+float chast1;
+float sins;
+float modul;
+float chast2;
+float kk;
+float cos2x;
+float chast3;
+float resh;
+
+int main() {
+    modul = abs(x - 5);
+
+    sins = sin(x);
+
+    kk = sqrt(x * x + 2014);
+
+    cos2x = cos(2 * x);
+
+    resh = (modul - sins) / 3 + kk * cos2x - 3;
+
+    cout << "Otvet = " << resh << endl;
+
+    getchar();  // ожидание ввода
+    getchar();
+    return 0;
+
+}
