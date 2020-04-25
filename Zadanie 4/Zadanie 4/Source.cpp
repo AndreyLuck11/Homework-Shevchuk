26dc34a6f7cd564ac2df30348442a39751e3a45d
AndreyLuck11 <61904447+AndreyLuck11@users.noreply.github.com>
2020-04-25 23:21:07 +0300

    Add files via upload

---
+++ Zadanie 4/Zadanie 4/Source.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct Plane {
+
+	string Country;
+	int NumberOfPassengers;
+	float Speed;
+};
+
+void randString(string& str) {
+	static const char alphabet[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+	str = "     ";
+
+
+	for (int i = 0; i < str.length(); i++) {
+		int r = rand() % (sizeof(alphabet) - 1);
+		str[i] = alphabet[r];
+	}
+	str[str.length()] = 0;
+}
+
+void showPlane(const Plane p) {
+
+	cout << "\t" << p.Country << "\t\t\t"
+		<< p.NumberOfPassengers << "\t\t"
+		<< "   " << p.Speed << endl;
+}
+
+void showPlaneF(const Plane p) {
+	fstream f;
+	f.open("data.txt", ios::out | ios::trunc);
+	f << "\t" << p.Country << "\t\t\t"
+		<< p.NumberOfPassengers << "\t\t"
+		<< "   " << p.Speed << endl;
+	f.close();
+
+}
+
+
+void bubbleSortSpeed(Plane planes[], int Size) {
+	int j = 0;
+	Plane tmp;
+	// идём по массиву
+	for (int i = 0; i < Size; i++) {
+		// делаем проверки в оставшейся части массива
+		for (int j = Size - 1; j >= i + 1; j--) {
+			if (planes[j].Speed > planes[i].Speed) {  // сравниваем соседние элементы
+				// делаем перестановку
+				tmp = planes[i];
+				planes[i] = planes[j];
+				planes[j] = tmp;
+			}
+		}
+	}
+}
+
+void bubbleSortNumberOfPassengers(Plane planes[], int Size) {
+	int j = 0;
+	Plane tmp;
+	// идём по массиву
+	for (int i = 0; i < Size; i++) {
+		// делаем проверки в оставшейся части массива
+		for (int j = Size - 1; j >= i + 1; j--) {
+			if (planes[j].NumberOfPassengers > planes[i].NumberOfPassengers) {  // сравниваем соседние элементы
+				// делаем перестановку
+				tmp = planes[i];
+				planes[i] = planes[j];
+				planes[j] = tmp;
+			}
+		}
+	}
+}
+
+
+void Cout(Plane planes[], int Size) {
+
+	cout << endl << "                        Sorted Max Speed" << endl << endl;
+	cout << "The brand of plane\t" << "Number Of Passengers\t" << "Max speed\n";
+	bubbleSortSpeed(planes, Size);
+
+	for (int i = 0; i < Size; i++)
+		showPlane(planes[i]);
+
+	cout << endl << "                 Sorted Number Of Passengers" << endl << endl;
+	cout << "The brand of plane\t" << "Number Of Passengers\t" << "Max speed\n";
+	bubbleSortNumberOfPassengers(planes, Size);
+
+	for (int i = 0; i < Size; i++)
+		showPlane(planes[i]);
+}
+
+void file(Plane planes[], int Size, const int l) {
+	
+	ofstream f;
+	f.open("data.txt");
+	for (int i = 0; i < l; i++) {
+		f << endl << "                        Sorted Max Speed" << endl << endl;
+		f << "The brand of plane\t" << "Number Of Passengers\t" << "Max speed\n";
+		bubbleSortSpeed(planes, Size);
+
+		for (int i = 0; i < Size; i++)
+			showPlaneF(planes[i]);
+
+		f << endl << "                 Sorted Number Of Passengers" << endl << endl;
+		f << "The brand of plane\t" << "Number Of Passengers\t" << "Max speed\n";
+		bubbleSortNumberOfPassengers(planes, Size);
+
+		for (int i = 0; i < Size; i++)
+			showPlaneF(planes[i]);
+	}
+	f.close();
+}
+
+void write2File(const Plane p[], const int l) {
+	ofstream f;
+	f.open("data.txt");
+
+
+	for (int i = 0; i < l; i++)
+		f << p[i].Country << "\t"
+		<< p[i].NumberOfPassengers << "\t"
+		<< p[i].Speed << endl;
+	f.close();
+}
+
+int main() {
+	int const Size = 15;
+	Plane planes[Size];
+	fstream f;
+	f.open("data.txt", ios::out | ios::trunc);
+	f << "The brand of plane\t" << "Number Of Passengers\t" << "Max speed\n";
+	for (int i = 0; i < Size; i++)
+	{
+		planes[i].NumberOfPassengers = 80 + rand() % 150;
+		planes[i].Speed = 725 + rand() % 250;
+		randString(planes[i].Country);
+
+		showPlaneF(planes[i]);
+
+
+	}
+	
+	cout << "The brand of plane\t" << "Number Of Passengers\t" << "Max speed\n";
+	for (int i = 0; i < Size; i++)
+	{
+		planes[i].NumberOfPassengers = 80 + rand() % 150;
+		planes[i].Speed = 725 + rand() % 250;
+		randString(planes[i].Country);
+
+		showPlane(planes[i]);
+
+
+	}
+
+
+	Cout(planes, Size);
+	file(planes, Size,  1);
+	write2File(planes, Size);
+
+
+
+
+
+
+	return 0;
+}
+
+
